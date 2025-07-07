f02261ff593206bc8f4fd88be2c885f38f1a2b6a
karl <karl.fahlbusch7241@gmail.com>
2025-07-07 12:14:57 -0500

    show details page

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,27 @@
+import { usestate } from "react";
+
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = usestate();
+
+  if (!show) {
+    return <p>Please select a show to learn more.</p>;
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
